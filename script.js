168df7982da2e11644e9fb4f23f74791f954e2ac
SK Maqshood <skmaqshood99@gmail.com>
2025-06-03 21:48:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,57 +1,24 @@
-// Person class
 class Person {
-  private _name: string;
-  private _age: number;
+	constructor(name,age){
+		this._name=name;
+		this._age=age;
+	}
 
-  constructor(name: string, age: number) {
-    this._name = name;
-    this._age = age;
-  }
-
-  // Getter for name
-  get name(): string {
-    return this._name;
-  }
-
-  // Setter for name
-  set name(name: string) {
-    this._name = name;
-  }
-
-  // Getter for age
-  get age(): number {
-    return this._age;
-  }
-
-  // Setter for age
-  set age(age: number) {
-    this._age = age;
-  }
+get name(){
+	return this._name
+}
+	set age(newAge){
+		this._age=newAge;
+	}
 }
 
-// Student class extending Person
-class Student extends Person {
-  study(): void {
-    console.log(`${this.name} is studying`);
-  }
-}
-
-// Teacher class extending Person
 class Teacher extends Person {
-  teach(): void {
-    console.log(`${this.name} is teaching`);
-  }
+	teach(){
+		console.log(${this._name} is teaching);
+	}
 }
 
-// Example test cases
-const person = new Person("John", 25);
-console.log(person.name); // Should output: John
-
-person.age = 30;
-console.log(person.age); // Should output: 30
-
-const student = new Student("John", 30);
-student.study(); // Should output: John is studying
-
-const teacher = new Teacher("John", 30);
-teacher.teach(); // Should output: John is teaching
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
